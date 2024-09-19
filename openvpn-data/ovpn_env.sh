6b343b4cb427fe969d23469fb1b77dfbbe970b3b
Omar Omar <oomar@umich.edu>
2024-09-19 03:36:39 -0400

    ok server is gtg

--- openvpn-data/ovpn_env.sh
+++ openvpn-data/ovpn_env.sh
@@ -1,14 +1,14 @@
 declare -x OVPN_AUTH=
 declare -x OVPN_CIPHER=
 declare -x OVPN_CLIENT_TO_CLIENT=
-#declare -x OVPN_CN=<IP/Domain>
+declare -x OVPN_CN=68.40.144.164
 declare -x OVPN_COMP_LZO=0
 declare -x OVPN_DEFROUTE=1
 declare -x OVPN_DEVICE=tun
 declare -x OVPN_DEVICEN=0
 declare -x OVPN_DISABLE_PUSH_BLOCK_DNS=0
 declare -x OVPN_DNS=1
-declare -x OVPN_DNS_SERVERS=("192.168.0.96")
+declare -x OVPN_DNS_SERVERS=([0]="192.168.0.96")
 declare -x OVPN_ENV=/etc/openvpn/ovpn_env.sh
 declare -x OVPN_EXTRA_CLIENT_CONFIG=()
 declare -x OVPN_EXTRA_SERVER_CONFIG=()
@@ -21,5 +21,5 @@ declare -x OVPN_PROTO=udp
 declare -x OVPN_PUSH=()
 declare -x OVPN_ROUTES=([0]="10.8.1.0/24")
 declare -x OVPN_SERVER=10.8.0.0/24
-#declare -x OVPN_SERVER_URL=udp://<IP/Domain>
+declare -x OVPN_SERVER_URL=udp://68.40.144.164
 declare -x OVPN_TLS_CIPHER=
